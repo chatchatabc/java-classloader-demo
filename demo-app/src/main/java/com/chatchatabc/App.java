41382f1ffe51ff987e333bd8acb6d56d799fa728
Libing Chen <libing.chen@gmail.com>
2022-12-23 21:51:28 +0800

    chore: add different classloaders

--- demo-app/src/main/java/com/chatchatabc/App.java
+++ demo-app/src/main/java/com/chatchatabc/App.java
@@ -8,8 +8,16 @@ import java.net.URLClassLoader;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        // platform classloader
+        final Class<?> stringClazz = ClassLoader.getPlatformClassLoader().loadClass("java.lang.String");
+        System.out.println(System.getProperty("java.home") + "/jre/lib/ext");
+        // system/app classloader
+        final Class<?> appClazz = ClassLoader.getSystemClassLoader().loadClass("com.chatchatabc.App");
         Class<UserService> clazz = UserService.class;
         final ClassLoader parentLoader = clazz.getClassLoader();
+        // system classloader to load application classes
+        assert parentLoader == ClassLoader.getSystemClassLoader();
+        //custom classloader
         final URL jarUrl = new File("jar-1/target/jar-1-1.0.0-SNAPSHOT.jar").toURI().toURL();
         try (URLClassLoader customLoader = new URLClassLoader(new java.net.URL[]{jarUrl}, parentLoader)) {
             final Class<?> jar1ImplClass = customLoader.loadClass("com.chatchatabc.jar1.UserServiceJar1Impl");
